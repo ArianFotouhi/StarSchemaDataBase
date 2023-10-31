afb820fe15fee7302ec8d74985097609c1efb7c7
ArianFotouhi <fotouhiarian@gmail.com>
2023-10-31 14:24:31 -0400

    v1.6

--- schema_metadata.py
+++ schema_metadata.py
@@ -1,10 +1,16 @@
 
 user_table_name = 'User'
 user_columns = {'UserID':'user_id', 'Email':'user_email', 'Password':'user_password'}
+user_types = {user_columns['UserID']:'TEXT PRIMARY KEY', user_columns['Email']:'TEXT', user_columns['Password']:'TEXT'}
 # ------------------------------------------------------------
-item_table_name = 'Table'
+item_table_name = 'Item'
 item_columns = {'ItemID':'item_id', 'Price':'item_price', 'ImageURL':'item_image_url'}
+item_types = {item_columns['ItemID']:'TEXT PRIMARY KEY', item_columns['Price']:'REAL', item_columns['ImageURL']:'TEXT'}
 # ------------------------------------------------------------
 tx_table_name= 'Transaction'
 tx_columns = {'TxID':'transaction_id', 'UserID': user_columns['UserID'], 'ItemID': item_columns['ItemID']}
+tx_types = {tx_columns['TxID']:'TEXT PRIMARY KEY', tx_columns['UserID']:'TEXT', tx_columns['ItemID']:'TEXT'}
 # ------------------------------------------------------------
+
+
+
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from utils import create_database_schema, insert_data_generic, fetch_transaction_data
-from schema_metadata import  user_table_name, item_table_name, tx_table_name, user_columns, item_columns, tx_columns
+from schema_metadata import  (user_table_name, item_table_name, tx_table_name,
+                               user_columns, item_columns, tx_columns,
+                               user_types, item_types, tx_types)
 
-user_id = 'user1'
-item_id = 'item1'
-transaction_id='trans1'
+user_id = 'user2'
+item_id = 'item2'
+transaction_id ='trans2'
 
 
 user_data = {user_columns['UserID']: user_id, 
@@ -22,27 +24,27 @@ transaction_data = {tx_columns['TxID']: transaction_id,
 # Create the database schema
 create_database_schema(
     table_name= user_table_name,
-    columns={user_columns['UserID']: 'TEXT PRIMARY KEY', 
-             user_columns['Email']: 'TEXT', 
-             user_columns['Password']: 'TEXT'}
+    columns={user_columns['UserID']: user_types[user_columns['UserID']], 
+             user_columns['Email']: user_types[user_columns['Email']], 
+             user_columns['Password']: user_types[user_columns['Password']]}
 )
 
 create_database_schema(
     table_name= item_table_name,
-    columns={item_columns['ItemID']: 'TEXT PRIMARY KEY',
-            item_columns['Price']: 'REAL',
-            item_columns['ImageURL']: 'TEXT'}
+    columns={item_columns['ItemID']: item_types[item_columns['ItemID']],
+            item_columns['Price']: item_types[item_columns['Price']],
+            item_columns['ImageURL']: item_types[item_columns['ImageURL']]}
 )
 
 create_database_schema(
     table_name= tx_table_name,
-    columns={tx_columns['TxID']: 'TEXT PRIMARY KEY', 
-             tx_columns['UserID']: 'TEXT', 
-             tx_columns['ItemID']: 'TEXT'},
+    columns={tx_columns['TxID']: tx_types[tx_columns['TxID']], 
+             tx_columns['UserID']: tx_types[tx_columns['UserID']], 
+             tx_columns['ItemID']: tx_types[tx_columns['ItemID']]},
+             
     foreign_keys={tx_columns['UserID']: f'User({user_columns["UserID"]})', 
                   tx_columns['ItemID']: f'Item({item_columns["ItemID"]})'}
 )
-
 
 
 
